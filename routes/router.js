fbae0bd5ec072507c5c86db6b8c1d873aa19fa3a
Corbin Robb <corbinrobb@gmail.com>
2020-06-12 10:23:14 -0600

    Meet MVP

---
+++ routes/router.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const db = require('../models/models.js');
+
+router.get('/projects', async (req, res) => {
+  try {
+    const projects = await db.getProjects();
+    res.status(200).json(projects);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({error: "Couldnt retrieve projects from database"})
+  }
+})
+
+router.get('/resources', async (req, res) => {
+  try {
+    const resources = await db.getResources();
+    res.status(200).json(resources);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Couldnt retrieve resources from database" })
+  }
+})
+
+router.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await db.getTasks();
+    res.status(200).json(tasks);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Couldnt retrieve tasks from database" })
+  }
+})
+
+router.post('/projects', async (req, res) => {
+  try {
+    const project = req.body;
+    
+    const projectId = await db.addProject(project);
+    res.status(200).json(projectId);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Couldnt add project to database" })
+  }
+})
+
+router.post('/resources', async (req, res) => {
+  try {
+    const resource = req.body;
+
+    const resourceId = await db.addResource(resource);
+    res.status(200).json(resourceId);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Couldnt add resource to database" })
+  }
+})
+
+router.post('/tasks', async (req, res) => {
+  try {
+    const task = req.body;
+
+    const taskId = await db.addTask(task);
+    res.status(200).json(taskId);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Couldnt add task to database" })
+  }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const server = express();
 
+const router = require('./routes/router.js');
+
 server.use(express.json());
 server.use(cors());
+
+server.use('/api', router);
 
 module.exports = server;
